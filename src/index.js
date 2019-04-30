ac875f2f118fc81da3e59668116e7d79a53346fc
David Arinder <david.arinder@gmail.com>
2019-04-30 13:12:14 -0700

    added task.js, updated index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 //This app involves setting up a rest API route -- rest API
+///home/david/mongodb/bin/mongod --dbpath=/home/david/mongodb-data
 
 const express = require('express')
 require('./db/mongoose')
 const User = require('./models/user')
+const Task = require('./models/task')
 
 const app = express()
 const port = process.env.PORT || 3000
@@ -13,11 +15,46 @@ app.post('/users', (req, res) => {
     const user = new User(req.body)
 
     user.save().then(() => {
-        res.send(user)
+        res.status(201).send(user)
     }).catch((e) => {
         res.status(400).send(e)
     })
 })
+
+app.get('/users', (req, res) => {
+    User.find({}).then((users) => {
+        res.send(users)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+//giving me 500 status when should be 404
+app.get('/users/:id', (req, res) => {
+    const _id = req.params.id
+
+    User.findById(_id).then((user) => {
+        if (!user) {
+            return res.status(404).send()
+        }
+
+        res.send(user)
+    }).catch((e) => {
+        res.status(500).send()
+    })
+})
+
+app.post('/tasks', (req, res) => {
+    const task = new Task(req.body)
+
+    task.save().then(() => {
+        res.status(201).send(task)
+    }).catch((e) => {
+        res.status(400).send(e)
+    })
+})
+
+
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
---
+++ src/models/task.js
@@ -0,0 +1,17 @@
+//tasks model
+
+const mongoose = require('mongoose')
+
+const Task = mongoose.model('Task', {
+    description: {
+        type: String,
+        required: true,
+        trim: true
+    }, 
+    completed: {
+        type: Boolean,
+        default: false
+    }
+})
+
+module.exports = Task
