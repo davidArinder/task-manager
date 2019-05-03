ef9f0cb584e61760c9d97fd144db5b30c3b5cb7d
David Arinder <david.arinder@gmail.com>
2019-05-03 12:18:47 -0700

    adding routers

--- src/index.js
+++ src/index.js
@@ -1,60 +1,18 @@
 //This app involves setting up a rest API route -- rest API
 ///home/david/mongodb/bin/mongod --dbpath=/home/david/mongodb-data
+//uses routing when setting up API endpoints etc
 
 const express = require('express')
 require('./db/mongoose')
-const User = require('./models/user')
-const Task = require('./models/task')
+const userRouter = require('./routers/user')
+const taskRouter = require('./routers/task')
 
 const app = express()
 const port = process.env.PORT || 3000
 
 app.use(express.json())
-
-app.post('/users', (req, res) => {
-    const user = new User(req.body)
-
-    user.save().then(() => {
-        res.status(201).send(user)
-    }).catch((e) => {
-        res.status(400).send(e)
-    })
-})
-
-app.get('/users', (req, res) => {
-    User.find({}).then((users) => {
-        res.send(users)
-    }).catch((e) => {
-        res.status(500).send()
-    })
-})
-
-//giving me 500 status when should be 404
-app.get('/users/:id', (req, res) => {
-    const _id = req.params.id
-
-    User.findById(_id).then((user) => {
-        if (!user) {
-            return res.status(404).send()
-        }
-
-        res.send(user)
-    }).catch((e) => {
-        res.status(500).send()
-    })
-})
-
-app.post('/tasks', (req, res) => {
-    const task = new Task(req.body)
-
-    task.save().then(() => {
-        res.status(201).send(task)
-    }).catch((e) => {
-        res.status(400).send(e)
-    })
-})
-
-
+app.use(userRouter)
+app.use(taskRouter)
 
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
