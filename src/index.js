2d9177dcf51bca029aec305382c0164031a3bcf8
David Arinder <david.arinder@gmail.com>
2019-05-05 11:43:44 -0700

    adding login functionality

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 //This app involves setting up a rest API route -- rest API
 ///home/david/mongodb/bin/mongod --dbpath=/home/david/mongodb-data
 //uses routing when setting up API endpoints etc
+//security: passwords (bcrypt.js hashing algorithm)
 
 const express = require('express')
 require('./db/mongoose')
@@ -17,3 +18,15 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
+
+const jwt = require('jsonwebtoken')
+
+const myFunction = async () => {
+    const token = jwt.sign({ _id: 'abc123' }, 'thisismyclass', { expiresIn: '7 days' })
+    console.log(token)
+
+    const data = jwt.verify(token, 'thisismyclass')
+    console.log(data)
+}
+
+myFunction()
--- src/models/task.js
+++ src/models/task.js
@@ -14,4 +14,16 @@ const Task = mongoose.model('Task', {
     }
 })
 
+// const taskSchema = new mongoose.Schema({
+//     description: {
+//         type: String,
+//         required: true,
+//         trim: true
+//     }, 
+//     completed: {
+//         type: Boolean,
+//         default: false
+//     }
+// })
+
 module.exports = Task
