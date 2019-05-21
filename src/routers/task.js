dbc6bb3f4c5772b9c3f1b4de7ea03405f4051b91
David Arinder <david.arinder@gmail.com>
2019-05-21 16:59:22 -0700

    added limit, skip, sort functionality

--- src/routers/task.js
+++ src/routers/task.js
@@ -19,18 +19,32 @@ router.post('/tasks', auth, async (req, res) => {
 })
 
 // fetch all tasks
-// GET /users?completed=true
+// GET /tasks?completed=true
+// GET /tasks?limit=10&skip=20
+// GET /tasks?sortBy=createdAt_desc
 router.get('/tasks', auth, async (req, res) => {
     const match = {}
+    const sort = {}
 
     if (req.query.completed) {
         match.completed = req.query.completed === 'true'
     }
 
+    // sort in asc or desc order
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split(':')
+        sort[parts[0]] = parts[1] === 'desc' ? -1 : 1
+    }
+
     try {
         await req.user.populate({
             path: 'tasks',
-            match
+            match,
+            options: {
+                limit: parseInt(req.query.limit), // limit results by number provided as "limit"
+                skip: parseInt(req.query.skip), // skip an amount of results
+                sort
+            }
         }).execPopulate()
         res.send(req.user.tasks)
     } catch (e) {
