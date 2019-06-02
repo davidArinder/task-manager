0bc3e1ffaf365dc7923f31f7213ab0dcf53e5762
David Arinder <david.arinder@gmail.com>
2019-06-02 14:18:34 -0700

    wiring up profile pictures

--- src/index.js
+++ src/index.js
@@ -8,19 +8,6 @@ const taskRouter = require('./routers/task')
 const app = express()
 const port = process.env.PORT || 3000
 
-// add middleware to run
-// app.use((req, res, next) => {
-//     if (req.method === 'GET') {
-//         res.send('GET requests are disabled')
-//     } else {
-//         next()
-//     }
-// })
-
-// app.use((req, res, next) => {
-//         res.status(503).send('Website under maintenance.')
-// })
-
 app.use(express.json())
 app.use(userRouter)
 app.use(taskRouter)
@@ -28,18 +15,3 @@ app.use(taskRouter)
 app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
-
-const Task = require('./models/task')
-const User = require ('./models/user')
-
-const main = async () => {
-    // const task = await Task.findById('5ce345c565007533086fd0f5')
-    // await task.populate('owner').execPopulate() // find user associated with task, task.owner now entire profile not just id
-    // console.log(task.owner)
-
-    // const user = await User.findById('5ce3457965007533086fd0f2')
-    // await user.populate('tasks').execPopulate()
-    // console.log(user.tasks)
-}
-
-main()
